84bcbd1c812af45fd83977d2127012beeb99195e
dentifrag <65632734+dentifrag@users.noreply.github.com>
2022-05-23 01:11:05 -0700

    Implemented more opcodes, still some bugs

--- src/chip.h
+++ src/chip.h
@@ -9,7 +9,7 @@
 using namespace std;
 
 class Chip {
-
+    sf::Event ev{};
     typedef unsigned char byte;
     typedef unsigned short twobytes;
 
@@ -20,25 +20,7 @@ class Chip {
     static const int HEX_SIZE = 16;
     static const int FNT_SIZE = 80;
 
-    const sf::Keyboard::Key key0x1 = sf::Keyboard::Key::Num1;
-    const sf::Keyboard::Key key0x2 = sf::Keyboard::Key::Num2;
-    const sf::Keyboard::Key key0x3 = sf::Keyboard::Key::Num3;
-    const sf::Keyboard::Key key0xC = sf::Keyboard::Key::Num4;
 
-    const sf::Keyboard::Key key0x4 = sf::Keyboard::Key::Q;
-    const sf::Keyboard::Key key0x5 = sf::Keyboard::Key::W;
-    const sf::Keyboard::Key key0x6 = sf::Keyboard::Key::E;
-    const sf::Keyboard::Key key0xD = sf::Keyboard::Key::R;
-
-    const sf::Keyboard::Key key0x7 = sf::Keyboard::Key::A;
-    const sf::Keyboard::Key key0x8 = sf::Keyboard::Key::S;
-    const sf::Keyboard::Key key0x9 = sf::Keyboard::Key::D;
-    const sf::Keyboard::Key key0xE = sf::Keyboard::Key::F;
-
-    const sf::Keyboard::Key key0xA = sf::Keyboard::Key::Z;
-    const sf::Keyboard::Key key0x0 = sf::Keyboard::Key::X;
-    const sf::Keyboard::Key key0xB = sf::Keyboard::Key::C;
-    const sf::Keyboard::Key key0xF = sf::Keyboard::Key::V;
 
 
 
@@ -51,7 +33,7 @@ public:
     bool drawFlag = false;
     twobytes chipStack[STK_SIZE]{};
     byte stackPointer{};
-    byte keypad[HEX_SIZE]{};
+    bool keypad[HEX_SIZE]{};
     twobytes pc{};
     twobytes indexRegister{};
     twobytes opcode{};
@@ -89,6 +71,29 @@ public:
     void dumpMemory();
 
     static int getNibble(twobytes opcode, int i);
+
+
+    const sf::Keyboard::Key key0x1 = sf::Keyboard::Key::Num1;
+    const sf::Keyboard::Key key0x2 = sf::Keyboard::Key::Num2;
+    const sf::Keyboard::Key key0x3 = sf::Keyboard::Key::Num3;
+    const sf::Keyboard::Key key0xC = sf::Keyboard::Key::Num4;
+
+    const sf::Keyboard::Key key0x4 = sf::Keyboard::Key::Q;
+    const sf::Keyboard::Key key0x5 = sf::Keyboard::Key::W;
+    const sf::Keyboard::Key key0x6 = sf::Keyboard::Key::E;
+    const sf::Keyboard::Key key0xD = sf::Keyboard::Key::R;
+
+    const sf::Keyboard::Key key0x7 = sf::Keyboard::Key::A;
+    const sf::Keyboard::Key key0x8 = sf::Keyboard::Key::S;
+    const sf::Keyboard::Key key0x9 = sf::Keyboard::Key::D;
+    const sf::Keyboard::Key key0xE = sf::Keyboard::Key::F;
+
+    const sf::Keyboard::Key key0xA = sf::Keyboard::Key::Z;
+    const sf::Keyboard::Key key0x0 = sf::Keyboard::Key::X;
+    const sf::Keyboard::Key key0xB = sf::Keyboard::Key::C;
+    const sf::Keyboard::Key key0xF = sf::Keyboard::Key::V;
+
+    void keyCheck();
 };
 
 #endif //CHIP_8_EMULATOR_CHIP_H
